08804dba5f9456de030d9c41e45d1e3d4c447680
RUDIWER <rudi.werner@pandora.be>
2019-01-07 12:41:43 +0100

    fix

--- app/ZawaClasses/PrestaApi.js
+++ app/ZawaClasses/PrestaApi.js
@@ -254,12 +254,12 @@ class PrestaApi {
 			console.log('error on store image to presta:', error);
 			console.log('Response from presta:', response && response.statusCode);
 			console.log('Response headers :', response.headers);
-			const coverImage = (await ps_Image.query().where('id_product', id).where('cover', '=', '1').fetch()).toJSON();
-			if (coverImage.length > 0) {
-				coverImage.cover = 1;
-				await coverImage.save();
-			}
 		});
+		const currentImage = (await ps_Image.query().where('id_product', id).where('cover', '=', '1').fetch()).toJSON();
+		if (currentImage.length > 0) {
+			currentImage.cover = 1;
+			await currentImage.save();
+		}
 	}
 
 	async setSupplier(id) {
