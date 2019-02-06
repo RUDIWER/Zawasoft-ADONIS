c8826555f0f748449ba5f8aeb108029cfdcbf43b
RUDIWER <rudi.werner@pandora.be>
2019-02-06 15:23:39 +0100

    fix error in storesquare catergories

--- database/migrations/1549279194905_storesquare_categories_schema.js
+++ database/migrations/1549279194905_storesquare_categories_schema.js
@@ -7,7 +7,7 @@ class StoreSquareCategorySchema extends Schema {
 		this.create('storesquare_categories', (table) => {
 			table.increments();
 			table.integer('id_storesquare').unsigned().notNullable().unique();
-			table.string('name', 80).notNullable();
+			table.string('name_nl', 80).notNullable();
 			table.decimal('cost_fix', 6, 2);
 			table.decimal('cost_procent', 6, 2);
 			table.timestamps();
